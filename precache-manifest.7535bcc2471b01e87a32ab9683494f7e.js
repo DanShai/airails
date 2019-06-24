70198e40d650faafd80aa601b45b57bdc0c77338
Dan Shai <danshai2013@gmail.com>
2019-06-24 06:00:50 +0300

    Updates

---
+++ precache-manifest.7535bcc2471b01e87a32ab9683494f7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3e3793b86e4e20405c7b0831bf8f04",
+    "url": "/airails/index.html"
+  },
+  {
+    "revision": "fb073f6a96a56f2204b4",
+    "url": "/airails/static/css/main.61d21350.chunk.css"
+  },
+  {
+    "revision": "e661462c7ccf09702a19",
+    "url": "/airails/static/js/2.991419ef.chunk.js"
+  },
+  {
+    "revision": "fb073f6a96a56f2204b4",
+    "url": "/airails/static/js/main.05fbb9f4.chunk.js"
+  },
+  {
+    "revision": "8d7e78a113897ee31279",
+    "url": "/airails/static/js/runtime~main.91423828.js"
+  },
+  {
+    "revision": "a819051151bd74ed77d6111dc14b3622",
+    "url": "/airails/static/media/rails.a8190511.mp4"
+  }
+]);
